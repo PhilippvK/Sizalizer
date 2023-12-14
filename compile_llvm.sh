fead3202997c85587f65bbdabb62436095f58b9f
hm-ahager <andreas.hager@hm.edu>
2023-12-14 14:22:56 +0000

    Building LLVM

--- compile_llvm.sh
+++ compile_llvm.sh
@@ -11,23 +11,24 @@ cd "$BUILD_DIR"
 
 # clang++: error: -gsplit-dwarf is unsupported with RISC-V linker relaxation (-mrelax)
 cmake -G Ninja \
-	-DLLVM_PARALLEL_LINK_JOBS=2 		\
-	-DLLVM_PARALLEL_COMPILE_JOBS=4 		\
-	-DLLVM_RAM_PER_COMPILE_JOB=5000 	\
-	-DLLVM_RAM_PER_LINK_JOB=15000		\
-	-DCMAKE_BUILD_TYPE=MinRelSize   \
-	-DCMAKE_C_Compiler="/opt/riscv/bin/clang" \
-	-DCMAKE_CXX_Compiler="/opt/riscv/bin/clang++" \
-	-DDEFAULT_SYSROOT="/opt/riscv/sysroot" \
-	-DGCC_INSTALL_PREFIX="/opt/riscv" 	\
-	-DLLVM_ENABLE_PROJECTS="clang;libc;lld;mlir"      \
-	-DLLVM_TARGET_ARCH="riscv32gc"  	\
-	-DLLVM_TARGETS_TO_BUILD="RISCV" 	\
-	-DLLVM_DEFAULT_TARGET_TRIPLE="riscv32-unknown-elf" \
-	-DLLVM_ENABLE_WARNINGS=ON			\
-	-DLLVM_ENABLE_WERROR=OFF			\
-	-DLLVM_USE_SPLIT_DWARF=OFF			\
-	-DLLVM_OPTIMIZED_TABLEGEN=ON		\
+	-DLLVM_PARALLEL_LINK_JOBS=2 						\
+	-DLLVM_PARALLEL_COMPILE_JOBS=4						\
+	-DLLVM_RAM_PER_COMPILE_JOB=5000 					\
+	-DLLVM_RAM_PER_LINK_JOB=15000						\
+	-DCMAKE_BUILD_TYPE=MinRelSize   					\
+	-DCMAKE_INSTALL_PREFIX="/lrv5"						\
+	-DDEFAULT_SYSROOT="/opt/riscv/sysroot" 				\
+	-DGCC_INSTALL_PREFIX="/opt/riscv" 					\
+	-DCMAKE_C_Compiler="/opt/riscv/bin/clang" 			\
+	-DCMAKE_CXX_Compiler="/opt/riscv/bin/clang++" 		\
+	-DLLVM_ENABLE_PROJECTS="clang;libc;lld;mlir"    	\
+	-DLLVM_TARGET_ARCH="riscv32gc"  					\
+	-DLLVM_TARGETS_TO_BUILD="RISCV" 					\
+	-DLLVM_DEFAULT_TARGET_TRIPLE="riscv32-unknown-elf" 	\
+	-DLLVM_ENABLE_WARNINGS=ON							\
+	-DLLVM_ENABLE_WERROR=OFF							\
+	-DLLVM_USE_SPLIT_DWARF=OFF							\
+	-DLLVM_OPTIMIZED_TABLEGEN=ON						\
 	"/llvm-project/llvm"
 
 cmake --build .
