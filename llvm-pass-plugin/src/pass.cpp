507bb696e7dc4828ed1cda0cf6cfa74d472987a6
andreas.hager@hm.edu <ahager@hm.edu>
2024-01-08 13:49:16 +0100

    Generating CFG and DFG

--- llvm-pass-plugin/src/pass.cpp
+++ llvm-pass-plugin/src/pass.cpp
@@ -1,3 +1,4 @@
+#include "llvm/IR/CFG.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
@@ -8,14 +9,88 @@
 #include <mgclient.h>
 
 using namespace llvm;
+using namespace std;
 
 namespace
 {
 
   struct MyPass : public PassInfoMixin<MyPass>
   {
+    string get_bb_name(BasicBlock *bb)
+    {
+      string str;
+      raw_string_ostream OS(str);
+      bb->printAsOperand(OS, false);
+      return OS.str();
+    }
 
+    string bb_to_string(BasicBlock *bb)
+    {
+      string str;
+      raw_string_ostream OS(str);
+      OS << *bb << "\n";
+      return OS.str();
+    }
 
+    string inst_to_string(Instruction *inst)
+    {
+      string str;
+      raw_string_ostream OS(str);
+      OS << *inst << "\n";
+      return OS.str();
+    }
+
+    string value_to_string(Value *value) {
+      string str;
+      raw_string_ostream OS(str);
+      OS << *value << "\n";
+      return OS.str();
+    }
+
+    void exec_qeury(mg_session *session, const char *query)
+    {
+      if (mg_session_run(session, query, NULL, NULL, NULL, NULL) < 0)
+      {
+        outs() << "failed to execute query: " << mg_session_error(session) << "\n";
+        mg_session_destroy(session);
+        exit(1);
+      }
+      if (mg_session_pull(session, NULL))
+      {
+        outs() << "failed to pull results of the query: " << mg_session_error(session) << "\n";
+        mg_session_destroy(session);
+        exit(1);
+      }
+
+      int status = 0;
+      mg_result *result;
+      int rows = 0;
+      while ((status = mg_session_fetch(session, &result)) == 1)
+      {
+        rows++;
+      }
+
+      if (status < 0)
+      {
+        outs() << "error occurred during query execution: " << mg_session_error(session) << "\n";
+      }
+      else
+      {
+        // printf("query executed successfuly and returned %d rows\n", rows);
+      }
+    }
+
+    void connect_bbs(mg_session *session, BasicBlock *first_bb, BasicBlock *second_bb)
+    {
+      // MERGE: create if not exist else match
+      string store_first = "MERGE (first_bb {name: '" + get_bb_name(first_bb) + "'})";
+      string set_frist_code = " SET first_bb.code =  '" + bb_to_string(first_bb) + "'";
+      string store_second = " MERGE (second_bb {name: '" + get_bb_name(second_bb) + "'})";
+      string set_second_code = " SET second_bb.code =  '" + bb_to_string(second_bb) + "'";
+      string rel = " MERGE (first_bb)-[:CFG]->(second_bb);";
+      string qry = store_first + set_frist_code + store_second + set_second_code + rel;
+      exec_qeury(session, qry.c_str());
+    }
 
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM)
     {
@@ -45,15 +120,49 @@ namespace
         exit(1);
       }
 
+      // Clear database
+      auto del = "MATCH (n) DETACH DELETE n;";
+      exec_qeury(session, del);
+
+      // mapp the var to the last bb name
+      map<string, string> var_to_bb;
+
       // Push CDFG to DB
       for (BasicBlock &bb : F)
       {
-        outs() << bb.getName() << "\n";
+        string bb_name = get_bb_name(&bb);
+        outs() << "Label: " << bb_name << "\n";
+
+        for (BasicBlock *suc_bb : successors(&bb))
+        {
+          connect_bbs(session, &bb, suc_bb);
+        }
+
         for (Instruction &inst : bb)
         {
-          outs() << "inst: "
-                 << inst
-                 << "\n";
+
+          string inst_str = inst_to_string(&inst);
+          outs() << "  " << inst_str;
+
+          Instruction::op_iterator opEnd = inst.op_end();
+          for (Instruction::op_iterator opi = inst.op_begin(); opi != opEnd; opi++)
+          {
+
+            Value *op = opi->get();
+            Type *tp = op->getType();
+            if (!tp->isLabelTy())
+            {
+              string src_str = value_to_string(op);
+              // outs() << "    - " << src_str;
+
+              // MERGE: create if not exist else match
+              string store_src = "MERGE (src_inst {name: '" + src_str + "'})";
+              string store_dst = " MERGE (dst_inst {name: '" + inst_str + "'})";
+              string rel = " MERGE (src_inst)-[:DFG]->(dst_inst);";
+              string qry = store_src + store_dst + rel;
+              exec_qeury(session, qry.c_str());
+            }
+          }
         }
       }
 
@@ -67,7 +176,8 @@ namespace
 
 } // end anonymous namespace
 
-PassPluginLibraryInfo getPassPluginInfo()
+PassPluginLibraryInfo
+getPassPluginInfo()
 {
   const auto callback = [](PassBuilder &PB)
   {
